0155a3691836deb988c79ffbaaeddadf7c46966a
Karl J. Overå <k@rl.run>
2023-04-19 16:16:46 +0200

    implement oppgaves for part 3
    Co-authored-by: Mikael Solstad <mikaelrinosolstad@gmail.com>

--- workshop/oppgaver/03-currying-partial-application/currying-partial-application.test.ts
+++ workshop/oppgaver/03-currying-partial-application/currying-partial-application.test.ts
@@ -4,7 +4,7 @@ import transactions from "../../data/transactions";
 import { EUR_TO_NOK } from "../constants";
 import { currencyFilter, toAmount } from "./__spoilers/dontopen";
 
-import { filterByCurrency } from "./currying-partial-application";
+import { dkkToSek, ninetyDkkInNok } from "./currying-partial-application";
 import * as CPA from "./currying-partial-application";
 
 describe("OPPGAVE 3.1", () => {
@@ -18,38 +18,26 @@ describe("OPPGAVE 3.1", () => {
       .slice(0, 5);
 
     expect(result).toEqual([
-      -176572.8336, -180234.66, -195101.136, -292522.99919999996,
-      -42670.547999999995,
+      -176572.8336, -180234.66, -195101.136, -292522.99919999996, -42670.547999999995,
     ]);
   });
 });
 
 describe("OPPGAVE 3.2", () => {
+  it("tell if transaction is the given currency", () => {
+    const isCurrency = CPA.isCurrencyCurried("NOK");
 
+    expect(isCurrency(transactions[0])).toBe(true);
+    expect(isCurrency(transactions[1])).toBe(false);
+  });
 });
 
-describe("TODO maybe use, maybe garb", () => {
-  it("filters out all transactions in NOK", () => {
-    expect(filterByCurrency("NOK", transactions)).toHaveLength(165);
+describe("OPPGAVE 3.3", () => {
+  it("convert from DKK to SEK", () => {
+    expect(dkkToSek(100)).toBe(154);
   });
 
-  it("filters out all transactions in SEK", () => {
-    expect(filterByCurrency("SEK", transactions)).toHaveLength(150);
-  });
-
-  it("filters out all transactions in DKK", () => {
-    expect(filterByCurrency("DKK", transactions)).toHaveLength(195);
-  });
-
-  it("filters out all transactions in EUR", () => {
-    expect(filterByCurrency("EUR", transactions)).toHaveLength(159);
-  });
-
-  it("filters out all transactions in USD", () => {
-    expect(filterByCurrency("USD", transactions)).toHaveLength(172);
-  });
-
-  it("filters out all transactions in GBP", () => {
-    expect(filterByCurrency("GBP", transactions)).toHaveLength(171);
+  it("should have the correct NOK", () => {
+    expect(Number(ninetyDkkInNok.toFixed(1))).toBe(151.3);
   });
 });
