ab97f225dd49e1f3bd48ac1088bcdfb7235e5b3d
Mikael Solstad <mikaelrinosolstad@gmail.com>
2023-04-26 16:19:50 +0200

    Fix exchangeRate

--- workshop/oppgaver/01-pure-functions/pure-functions.test.ts
+++ workshop/oppgaver/01-pure-functions/pure-functions.test.ts
@@ -13,23 +13,23 @@ import { convertCurrency, isCurrency, takeFirstN } from "./pure-functions";
 
 describe("OPPGAVE 1.1", () => {
   it("correctly converts from DKK to SEK", () => {
-    expect(convertCurrency(100, DKK_TO_SEK)).toEqual(154);
+    expect(convertCurrency(DKK_TO_SEK, 100)).toEqual(154);
   });
 
   it("correctly converts from SEK to USD", () => {
-    expect(convertCurrency(100, SEK_TO_USD)).toEqual(9.4);
+    expect(convertCurrency(SEK_TO_USD, 100)).toEqual(9.4);
   });
 
   it("correctly converts from USD to GBP", () => {
-    expect(convertCurrency(100, USD_TO_GBP)).toEqual(82);
+    expect(convertCurrency(USD_TO_GBP, 100)).toEqual(82);
   });
 
   it("correctly converts from GBP to EUR", () => {
-    expect(convertCurrency(100, GBP_TO_EUR)).toEqual(112.99999999999999);
+    expect(convertCurrency(GBP_TO_EUR, 100)).toEqual(112.99999999999999);
   });
 
   it("correctly converts from EUR to NOK", () => {
-    expect(convertCurrency(100, EUR_TO_NOK)).toEqual(1128);
+    expect(convertCurrency(EUR_TO_NOK, 100)).toEqual(1128);
   });
 });
 
